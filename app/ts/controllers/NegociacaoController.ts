f037070ffda7256c2879d19d28371098cc8cee2b
Leticia Corral <le.martincorral@gmail.com>
2020-04-21 14:19:38 -0300

    implementando async

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -55,32 +55,32 @@ export class NegociacaoController {
     }
 
     @throttle()
-    importaDados() {
+    async importaDados() {
 
-        this._service
+        try {
+            const negociacoesParaImportar = await this._service
             .obterNegociacoes(res => {
                 if(res.ok) {
                     return res;
                 } else {
                     throw new Error(res.statusText);
                 }
-            })
-            .then(negociacoesParaImportar => {
-
-                const negociacoesJaImportadas = this._negociacoes.paraArray();
-
-                negociacoesParaImportar
-                    .filter(negociacao => 
-                        !negociacoesJaImportadas.some(jaImportada => 
-                            negociacao.ehIgual(jaImportada)))
-                    .forEach(negociacao => 
-                        this._negociacoes.adiciona(negociacao));
-                        
-                this._negociacoesView.update(this._negociacoes);
-            })
-            .catch(err => {
-                this._mensagemView.update(err.message);
             });
+
+        const negociacoesJaImportadas = this._negociacoes.paraArray();
+
+        negociacoesParaImportar
+            .filter(negociacao => 
+                !negociacoesJaImportadas.some(jaImportada => 
+                    negociacao.ehIgual(jaImportada)))
+            .forEach(negociacao => 
+                this._negociacoes.adiciona(negociacao));
+                
+        this._negociacoesView.update(this._negociacoes);
+
+        } catch(err) {
+            this._mensagemView.update(err.message);
+        }
     } 
 }
 
